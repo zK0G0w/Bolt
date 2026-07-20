7b28cb05b090766ed4e1283e035c0a00cf4c1486
WainZeng <zk0g0w@gmail.com>
2026-07-20 18:27:48 +0800

    feat: 添加 Spring Boot 启动入口
    WainZeng

---
+++ src/main/java/top/wain/bolt/BoltApplication.java
@@ -0,0 +1,17 @@
+package top.wain.bolt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: RTB 广告竞价引擎启动入口
+ * @Author: WainZeng
+ * @Date: 2026/07/20
+ */
+@SpringBootApplication
+public class BoltApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoltApplication.class, args);
+    }
+}
